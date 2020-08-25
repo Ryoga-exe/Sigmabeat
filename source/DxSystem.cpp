b851868f3c757ce3a2b9ba858f78ac2e3d2e6608
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-08-26 03:04:42 +0900

    Add some functions

--- source/DxSystem.cpp
+++ source/DxSystem.cpp
@@ -1,4 +1,5 @@
 #include "DxSystem.h"
+#include <cstdarg>
 
 void ErrMsgBx(const TCHAR* errorMsg) {
     MessageBox(NULL, errorMsg, "ERROR", MB_OK | MB_ICONERROR);
@@ -127,6 +128,27 @@ int  DrawBg(unsigned int color) {
     RectSize winSize = DxSystem::Inst()->GetWindowSize();
     return DxLib::DrawBox(0, 0, winSize.width, winSize.height, color, TRUE);
 }
+int  DrawBg(int grHandle, ColorRect_t color, int isTrans) {
+    DxLib::VERTEX2D m_vertex[4];
+    const WORD m_index[6] = { 0,1,2,2,3,0 };
+    const DxLib::COLOR_U8 m_colorTable[4] = {
+        color.col1.GetU8(255), color.col2.GetU8(255), color.col3.GetU8(255), color.col4.GetU8(255)
+    };
+    RectSize winSize = DxSystem::Inst()->GetWindowSize();
+    const DxLib::VECTOR m_posTable[4] = {
+        {0.f, 0.f, 0.f}, {(float)winSize.width , 0.f, 0.f},
+        {(float)winSize.width, (float)winSize.height, 0.f}, {0.f, (float)winSize.height, 0.f}
+    };
+    for (int i = 0; i < 4; i++) {
+        m_vertex[i].u = m_vertex[i].v = 1.f;
+        m_vertex[i].rhw = 1.f;
+        m_vertex[i].dif = m_colorTable[i];
+        m_vertex[i].pos = m_posTable[i];
+    }
+    m_vertex[0].u = m_vertex[0].v = m_vertex[1].v = m_vertex[3].u = 0.f;
+    m_vertex[1].u = m_vertex[2].u = m_vertex[2].v = m_vertex[3].v = 1.f;
+    return DxLib::DrawPolygonIndexed2D(m_vertex, 4, m_index, 2, grHandle, isTrans);
+}
 int  GetFileNum(const TCHAR* path, bool doCountDir) {
     int fileNum = 0;
     FILEINFO fileInfo;
@@ -155,4 +177,91 @@ bool FileRead_isBr(const int* fileHandle, TCHAR ch) {
         }
     }
     return false;
+}
+int  DrawStringToHandleCenter(int y, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor, int verticalFlag) {
+    return DxLib::DrawStringToHandle((DxSystem::Inst()->GetWindowSize().width - DxLib::GetDrawStringWidthToHandle(string, -1, fontHandle)) / 2, y, string, color, fontHandle, edgeColor, verticalFlag);
+}
+int  DrawStringFToHandleCenter(float y, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor, int verticalFlag) {
+    return DxLib::DrawStringFToHandle((DxSystem::Inst()->GetWindowSize().width - DxLib::GetDrawStringWidthToHandle(string, -1, fontHandle)) / 2.f, y, string, color, fontHandle, edgeColor, verticalFlag);
+}
+int  DrawStringToHandleRight(int y, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor, int verticalFlag) {
+    return DxLib::DrawStringToHandle((DxSystem::Inst()->GetWindowSize().width - DxLib::GetDrawStringWidthToHandle(string, -1, fontHandle)), y, string, color, fontHandle, edgeColor, verticalFlag);
+}
+int  DrawStringToHandleBoxCenter(int x1, int y1, int x2, int y2, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor, int verticalFlag) {
+    int sizeX, sizeY, lineNum;
+    int _x1 = x1, _y1 = y1, _x2 = x2, _y2 = y2;
+    if (x1 > x2) {
+        _x1 = x2;
+        _x2 = x1;
+    }
+    if (y1 > y2) {
+        _y1 = y2;
+        _y2 = y1;
+    }
+    DxLib::GetDrawStringSizeToHandle(&sizeX, &sizeY, &lineNum, string, -1, fontHandle);
+    return DxLib::DrawStringToHandle(_x1 + ((_x2 - _x1) - sizeX) / 2, _y1 + ((_y2 - _y1) - sizeY) / 2, string, color, fontHandle, edgeColor, verticalFlag);
+}
+int  DrawZoomStringToHandleBoxCenter(int x1, int y1, int x2, int y2, double zoomRate, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor, int verticalFlag) {
+    int sizeX, sizeY, lineNum;
+    int _x1 = x1, _y1 = y1, _x2 = x2, _y2 = y2;
+    if (x1 > x2) {
+        _x1 = x2;
+        _x2 = x1;
+    }
+    if (y1 > y2) {
+        _y1 = y2;
+        _y2 = y1;
+    }
+    DxLib::GetDrawStringSizeToHandle(&sizeX, &sizeY, &lineNum, string, -1, fontHandle);
+    return DxLib::DrawExtendStringToHandle((int)(_x1 + ((_x2 - _x1) - sizeX * zoomRate) / 2), (int)(_y1 + ((_y2 - _y1) - sizeY * zoomRate) / 2), zoomRate, zoomRate, string, color, fontHandle, edgeColor, verticalFlag);
+}
+int  DrawFormatStringToHandleCenter(int y, unsigned int color, int fontHandle, const TCHAR* string, ...) {
+    va_list argp;
+    va_start(argp, string);
+    TCHAR pszBuf[256];
+    vsnprintfDx(pszBuf, sizeof(pszBuf), string, argp);
+    va_end(argp);
+    return DrawStringToHandleCenter(y, pszBuf, color, fontHandle, 0U, FALSE);
+}
+int  DrawZoomStringToHandleCenter(int y, double zoomRate, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor, int verticalFlag) {
+    return DxLib::DrawExtendStringToHandle((int)((DxSystem::Inst()->GetWindowSize().width - (DxLib::GetDrawStringWidthToHandle(string, -1, fontHandle) * zoomRate)) / 2), y, zoomRate, zoomRate, string, color, fontHandle, edgeColor, verticalFlag);
+}
+int  DrawZoomStringFToHandleCenter(float y, double zoomRate, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor, int verticalFlag) {
+    return DxLib::DrawExtendStringFToHandle((float)(DxSystem::Inst()->GetWindowSize().width - (DxLib::GetDrawStringWidthToHandle(string, -1, fontHandle) * zoomRate)) / 2.f, y, zoomRate, zoomRate, string, color, fontHandle, edgeColor, verticalFlag);
+}
+int  DrawRhombus(int x, int y, int r, unsigned int color, int fillFlag) {
+    return DxLib::DrawQuadrangle(x, y - r, x - r, y, x, y + r, x + r, y, color, TRUE);
+}
+int  DrawRhombusAA(float x, float y, float r, unsigned int color, int fillFlag, float lineThickness) {
+    return DxLib::DrawQuadrangleAA(x, y - r, x - r, y, x, y + r, x + r, y, color, fillFlag, lineThickness);
+}
+int  DrawGaugeBoxAA(float x1, float y1, float x2, float y2, int currentValue, int maxValue, unsigned int color, int direction) {
+    float _x1 = x1, _y1 = y1, _x2 = x2, _y2 = y2;
+    if (x1 > x2) {
+        _x1 = x2;
+        _x2 = x1;
+    }
+    if (y1 > y2) {
+        _y1 = y2;
+        _y2 = y1;
+    }
+    if (currentValue > maxValue) currentValue = maxValue;
+    return DxLib::DrawBoxAA(_x1, _y1, _x1 + (_x2 - _x1) * currentValue / maxValue, _y2, color, TRUE);
+}
+int  DrawGraphOnPolygon3DRect(DxLib::VECTOR pos1, DxLib::VECTOR pos2, int grHandle, int transFlag) {
+    DxLib::VERTEX3D vertex[6];
+    for (int i = 0; i < 6; i++) {
+        vertex[i].su = vertex[i].sv = 0.f;
+        vertex[i].norm = DxLib::VGet(0.f, 0.f, -1.f);
+        vertex[i].dif = DxLib::GetColorU8(255, 255, 255, 255);
+        vertex[i].spc = DxLib::GetColorU8(0, 0, 0, 255);
+    }
+    vertex[0].u = vertex[0].v = vertex[1].v = vertex[2].u = vertex[4].u = vertex[5].v = 0.f;
+    vertex[1].u = vertex[2].v = vertex[3].u = vertex[3].v = vertex[4].v = vertex[5].u = 1.f;
+
+    vertex[0].pos = pos1;
+    vertex[1].pos = vertex[5].pos = DxLib::VGet(pos2.x, pos1.y, pos1.z);
+    vertex[2].pos = vertex[4].pos = DxLib::VGet(pos1.x, pos2.y, pos2.z);
+    vertex[3].pos = pos2;
+    return DxLib::DrawPolygon3D(vertex, 2, grHandle, TRUE);
 }
--- source/DxSystem.h
+++ source/DxSystem.h
@@ -48,5 +48,18 @@ private:
 
 int  operator""_sec(const long double second);
 int  DrawBg(unsigned int color);
+int  DrawBg(int grHandle, ColorRect_t color, int isTrans);
 int  GetFileNum(const TCHAR* path, bool doCountDir = false);
 bool FileRead_isBr(const int* fileHandle, TCHAR ch);
+int  DrawStringToHandleCenter(int y, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor = 0U, int verticalFlag = FALSE);
+int  DrawStringFToHandleCenter(float y, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor = 0U, int verticalFlag = FALSE);
+int  DrawStringToHandleRight(int y, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor = 0U, int verticalFlag = FALSE);
+int  DrawStringToHandleBoxCenter(int x1, int y1, int x2, int y2, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor = 0U, int verticalFlag = FALSE);
+int  DrawZoomStringToHandleBoxCenter(int x1, int y1, int x2, int y2, double zoomRate, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor = 0U, int verticalFlag = FALSE);
+int  DrawFormatStringToHandleCenter(int y, unsigned int color, int fontHandle, const TCHAR* string, ...);
+int  DrawZoomStringToHandleCenter(int y, double zoomRate, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor = 0U, int verticalFlag = FALSE);
+int  DrawZoomStringFToHandleCenter(float y, double zoomRate, const TCHAR* string, unsigned int color, int fontHandle, unsigned int edgeColor = 0U, int verticalFlag = FALSE);
+int  DrawRhombus(int x, int y, int r, unsigned int color, int fillFlag);
+int  DrawRhombusAA(float x, float y, float r, unsigned int color, int fillFlag, float lineThickness = 1.0f);
+int  DrawGaugeBoxAA(float x1, float y1, float x2, float y2, int currentValue, int maxValue, unsigned int color, int direction = 0);
+int  DrawGraphOnPolygon3DRect(DxLib::VECTOR pos1, DxLib::VECTOR pos2, int grHandle, int transFlag);
